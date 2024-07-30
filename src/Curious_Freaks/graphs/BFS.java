6b81436a3f7599909105cf9fedeec46be826eede
Jothiprakash T <jothiprakash888@gmail.com>
2024-07-30 09:02:54 +0530

    Merge pull request #22
    * upto july 29 evening session committed
    
    * upto july 30 morning session committed

---
+++ src/Curious_Freaks/graphs/BFS.java
@@ -0,0 +1,51 @@
+package Curious_Freaks.graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    public static void main(String[] args) {
+
+        BFS obj = new BFS();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        int V = 5;
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<Integer>());
+        }
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+        adj.get(1).add(2);
+        adj.get(2).add(0);
+        adj.get(2).add(3);
+        adj.get(3).add(4);
+
+        ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+        for (int k :
+                ans) {
+            System.out.print(k + " ");
+
+        }
+    }
+
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> bfs = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[V];
+        q.add(0);
+        visited[0] = true;
+        while (!q.isEmpty()) {
+            int Node = q.poll();
+            bfs.add(Node);
+            for (int i : adj.get(Node)) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+        return bfs;
+
+    }
+}
