d5d04285357eac7b92e0e7ad403cab22c945a373
jothiprakash <jothiprakash888@gmail.com>
2024-08-04 08:35:30 +0530

    upto August 04 morning session committed

---
+++ src/Curious_Freaks/BinarySearch/SingleElementInArray.java
@@ -0,0 +1,35 @@
+package Curious_Freaks.BinarySearch;
+
+public class SingleElementInArray {
+    public static void main(String[] args) {
+        SingleElementInArray singleElementInArray = new SingleElementInArray();
+        int[] arr = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7};
+        int res = singleElementInArray.searchSingleElement(arr);
+        System.out.println(res + " hl ");
+    }
+
+    int searchSingleElement(int[] arr) {
+        int ans = -1;
+
+        if (arr.length == 1) {
+            return arr[0];
+        }
+
+        if (arr[0] != arr[1]) {
+            return arr[0];
+        }
+
+        if (arr[arr.length - 1] != arr[arr.length - 2]) {
+            return arr[arr.length - 1];
+        }
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1]) {
+                ans = arr[i];
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
